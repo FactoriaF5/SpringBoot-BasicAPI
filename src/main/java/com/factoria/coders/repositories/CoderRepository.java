0e2b61f81213ebf068d0fecf83bca679df6a600e
Sergi Virgili <svirgilif@gmail.com>
2022-06-20 12:13:52 +0200

    add find coders by Id

--- src/main/java/com/factoria/coders/repositories/CoderRepository.java
+++ src/main/java/com/factoria/coders/repositories/CoderRepository.java
@@ -7,21 +7,21 @@ import java.util.List;
 
 
 public class CoderRepository {
-//    private List<Coder> getCoderList() {
-//        return List.of
-//                (new Coder("Alex",1L),
-//                        new Coder("marta", 2L),
-//                        new Coder("Alex", 3L));
-//    }
-//
-//    public List<Coder> findAll() {
-//        return this.getCoderList();
-//    }
-//
-//    public Coder findById(Long id) {
-//        var list = getCoderList();
-//
-//        var a = list.stream().filter(x -> x.getId() == id).findFirst().get();
-//        return a;
-//    }
+    private List<Coder> getCoderList() {
+        return List.of
+                (new Coder("Alex",1L),
+                        new Coder("marta", 2L),
+                        new Coder("Alex", 3L));
+    }
+
+    public List<Coder> findAll() {
+        return this.getCoderList();
+    }
+
+    public Coder findById(Long id) {
+        var list = getCoderList();
+
+        var a = list.stream().filter(x -> x.getId() == id).findFirst().get();
+        return a;
+    }
 }
